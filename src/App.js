e01ef9135d7ca74c9fdde5313b8a5870c3a77ad1
kauanherminio225 <143223965+kauanherminio225@users.noreply.github.com>
2023-08-25 18:23:37 -0300

    inicio do projeto

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function Meuperfil() {
+  return (
+    <div>
+      <h1>kauan herminio </h1>
+      <h2>gosto de jogar videogames, estudar, me divertir com os amigos e adoro fazer sobremesas</h2>
+ <p>
+
+ </p>
+ <img src="blob:https://web.whatsapp.com/ed6402a1-62ba-49fa-a02f-54148e74f816"/>
+    </div>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>bem vindo ao meu perfil</h1>
+      <Meuperfil />
+    </div>
+  );
+}
